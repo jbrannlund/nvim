ec51c00572c0ea2d56436e1ef9917c19c85a6628
JB <johan.brannlund1@gmail.com>
2024-11-10 16:53:39 +0100

    startup.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -48,5 +48,12 @@ return {
             require("ibl").setup()  -- Adjusted to call ibl's setup function
         end,
     },
+    {
+    "startup-nvim/startup.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
+    config = function()
+      require "startup".setup()
+  end
+}
 }
 
